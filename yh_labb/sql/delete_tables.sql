bd3147fc9c7d2ce47f33fd0fd2191c0c93cd4822
Charlotta <charlotta.dellborb@hotmail.se>
2026-02-10 15:35:01 +0100

    script for deleting tables to start over if wanted

---
+++ yh_labb/sql/delete_tables.sql
@@ -0,0 +1,12 @@
+SET search_path TO yrkesco_database;
+
+DROP TABLE Student;
+DROP TABLE SchoolClass;
+DROP TABLE FreestandingCourse;
+DROP TABLE ProgrammeCourse;
+DROP TABLE Course;
+DROP TABLE Programme;
+DROP TABLE Consultant;
+DROP TABLE PermanentEmployee;
+DROP TABLE Staff;
+DROP TABLE SchoolLocation;
