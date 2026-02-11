af68e255ce6eb5f70ca373eb1b48e8fec73dcebe
Charlotta <charlotta.dellborb@hotmail.se>
2026-02-11 16:30:40 +0100

    Added script for querying class and a folder for the query scripts

---
+++ yh_labb/sql/query/query_class.sql
@@ -0,0 +1,15 @@
+SET
+    search_path TO yrkesco_database;
+
+SELECT
+    SchoolClass.class_id,
+    Programme.programme_name,
+    SchoolLocation.city,
+    Staff.first_name || ' ' || Staff.last_name AS director
+FROM
+    SchoolClass
+    INNER JOIN Programme USING (programme_code)
+    INNER JOIN SchoolLocation USING (location_id)
+    INNER JOIN Staff ON (Programme.director=Staff.staff_id)
+WHERE
+    class_id = 'DE25A';
