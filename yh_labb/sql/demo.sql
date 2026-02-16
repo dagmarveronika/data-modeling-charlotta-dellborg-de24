0e3d26533ea0a42fc81a0b339efe5f728174a1d0
Charlotta <charlotta.dellborb@hotmail.se>
2026-02-16 17:55:38 +0100

    select all from all tables added

---
+++ yh_labb/sql/demo.sql
@@ -0,0 +1,12 @@
+SET search_path TO yrkesco_database;
+
+SELECT * FROM Student;
+SELECT * FROM SchoolClass;
+SELECT * FROM SchoolLocation;
+SELECT * FROM Programme;
+SELECT * FROM Course;
+SELECT * FROM ProgrammeCourse;
+SELECT * FROM FreestandingCourse;
+SELECT * FROM Staff;
+SELECT * FROM PermanentEmployee;
+SELECT * FROM Consultant;
